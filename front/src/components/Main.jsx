92866b7299efb2593dbbfdeae20668a7bca47a55
JJW <mmd011375@gmail.com>
2024-12-19 17:51:00 +0900

    지도 추가 좌표 이동하기

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -1,26 +1,29 @@
 "use client"
 
-import { Status, Wrapper } from "@googlemaps/react-wrapper"
+import { useRef, useState } from "react"
+import Map from "./Map/Map";
 
 export default function Main() {
-    
-    const render = () => {
-        switch(Status) {
-            case Status.LOADING:
-                return <>Loading...</>
-            case Status.FAILURE:
-                return <>Error!</>
-            case Status.SUCCESS:
-                return <>Loading Successfully!</>
-        }
+
+    const [location,setLocation] = useState(null);
+    const btnRef = useRef();
+
+    const onChangeLocation = (e) => {
+        const lat = e.target.dataset.lat;
+        console.log(lat);
     }
+
+    console.log(location);
+
     return (
         <>
             <div>메인 페이지 입니다.</div>
-            <Wrapper
-                apiKey="AIzaSyDY4kZ-eJurK-Uv1sO-IXdpizQywV38ezs"
-                render={render}
-            ></Wrapper>
+            <div>
+                <button data-lat="35.6895" data-lng="139.6917" onClick={onChangeLocation}>도쿄</button>
+                <button value={{lat: 37.5665, lng: 126.9780}} onClick={onChangeLocation}>서울</button>
+                <button value={{lat: 40.7128, lng: -74.0060}} onClick={onChangeLocation}>뉴욕</button>
+            </div>
+            {location && <Map Location={location} />}
         </>
     )
 }
